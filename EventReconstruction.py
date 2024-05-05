6875d89c928b469f1ffd07a060bfc99a4cf6c07a
Davide Basilico <davide.basilico@mi.infn.it>
2024-05-05 11:31:42 +0200

    v0.1.0
    WF Analysis: input data batched with uproot
    Event Reco: completely re-written
    
    x12 speed improvement for WF Analysis
    > x100 speed improvement for Event Reconstruction

--- EventReconstruction.py
+++ EventReconstruction.py
@@ -1,245 +1,108 @@
-# -*- coding: utf-8 -*-
-# OSIRIS EVENT RECONSTRUCTION
-# Author: Davide Basilico davide.basilico@mi.infn.it  2024 March 27
-
 import numpy as np
-import uproot
+import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-from scipy.optimize import curve_fit
-import matplotlib.colors as mcolors
+import uproot
+from tqdm import tqdm
+from tqdm.auto import tqdm
+tqdm.pandas()
+
+c = 3.0e8
+n_LS = 1.55
+
+print("### Event Reconstruction ###")		
+
+def weighted_center(group, coord):
+    return (group[coord] * group['charge'].abs()).sum() / group['charge'].abs().sum()
+
 plt.rcParams.update({'font.size': 12})
 
-def y_fit(x, a, b,c,d):			# function I used to fit the firedPMT-charge scatter plot -- can be changed, not really needed
-    return (a*x*x+b*x+c+np.exp(d*x)) 
-    
-occurrences = {}
-sums = {}
-occurrences_HG = {}
-sums_HG = {}
-sums_HG_CN = {}
-x_CM_per_numero = {}
-y_CM_per_numero = {}
-z_CM_per_numero = {}
-Charge_Norm = {}
+data = pd.read_csv(sys.argv[1], delimiter='\t')
 
-#### READING THE WF ANALYZER OUTPUT
-data = np.genfromtxt(sys.argv[1], skip_header=0)  #in case, skip the first line which should be an header and change skip_header=0
-NameFile = data[:, 0]
-Date = data[:, 1]
-index = data[:, 2]
-charge = data[:, 3]
-WF_RiseTime = data[:, 5]
-trgNsec = data[:, 6]
-ID_channel = data[:, 7]
-xx = data[:, 9]
-yy = data[:, 10]
-zz = data[:, 11]
-LivePMTs = data[:, 12]
+grouped = data.groupby('index')
+total_charge = -grouped['charge'].sum()
+average_live_pmts = grouped['LivePMTs'].mean()
+normalized_charge = total_charge / average_live_pmts
+Fired_PMTs = grouped.size()
 
-for numero in np.unique(index):
-    indici = index == numero
-    x_CM = np.sum(charge[indici] * xx[indici]) / np.sum(charge[indici])
-    x_CM_per_numero[numero] = x_CM
-    y_CM = np.sum(charge[indici] * yy[indici]) / np.sum(charge[indici])
-    y_CM_per_numero[numero] = y_CM
-    z_CM = np.sum(charge[indici] * zz[indici]) / np.sum(charge[indici])
-    z_CM_per_numero[numero] = z_CM
-    print(numero, np.sum(charge[indici]/LivePMTs[indici]), x_CM,y_CM,z_CM)
+weights = data['charge'].abs()
 
-WF_RiseTime_diff_aligned = np.zeros_like(WF_RiseTime)
-WF_RiseTime_diff = np.zeros_like(WF_RiseTime)
-TOF = np.zeros_like(WF_RiseTime)
+print("-- Position reco based on charge barycenter")
+x_CM = (data['x_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
+y_CM = (data['y_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
+z_CM = (data['z_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
 
-Multiplicity_Cut = 2
-unique_indices, counts = np.unique(index, return_counts=True)
-groups_to_process = unique_indices[counts > Multiplicity_Cut]
+data = data.merge(x_CM.rename('x_CM'), on='index')
+data = data.merge(y_CM.rename('y_CM'), on='index')
+data = data.merge(z_CM.rename('z_CM'), on='index')
 
-TOF = np.zeros_like(WF_RiseTime)
-WF_RiseTime_diff = np.zeros_like(WF_RiseTime)
-WF_RiseTime_diff_aligned = np.zeros_like(WF_RiseTime)
-min_WF_RiseTime_per_evento = np.zeros_like(WF_RiseTime)
+print("-- TOF calculation")
+data['TOF'] = np.sqrt((data['x_CM'] - data['x_PMT'])**2 +
+                      (data['y_CM'] - data['y_PMT'])**2 +
+                      (data['z_CM'] - data['z_PMT'])**2) / 1000 / n_LS / c * 1.0e9
 
-#### Calculation of rise time
-for numero in groups_to_process:
-    indici = index == numero
-    if np.sum(indici) > Multiplicity_Cut:   
-        min_WF_RiseTime_per_evento[indici] = np.min(WF_RiseTime[indici])
+data['WF_RiseTime_diff'] = data['WF_RiseTime'] - data['TOF']                      
 
-#### Calculation of TOF and rise time differences (only for selected events)
-for i in range(len(WF_RiseTime)):
-    if index[i] in groups_to_process:
-        TOF[i] = np.sqrt((x_CM_per_numero[index[i]] - xx[i])**2 + (y_CM_per_numero[index[i]] - yy[i])**2 + (z_CM_per_numero[index[i]] - zz[i])**2) / 1000 / (1.55) / 3.0e8 * 1.0e9
+TOF = data.groupby('index')['TOF'].apply(list)
+WF_RiseTime_diff_per_event = data.groupby('index')['WF_RiseTime_diff'].apply(list)
+WF_RiseTime_per_event = data.groupby('index')['WF_RiseTime'].apply(list)
 
-        WF_RiseTime_diff[i] = WF_RiseTime[i] - TOF[i]
-        WF_RiseTime_diff_aligned[i] = WF_RiseTime_diff[i] - min_WF_RiseTime_per_evento[i]
-  
-#### Calculation of number of fired PMTs and integrated charge for each event
-for i in range(len(index)):
-    idx = int(index[i])
-    occurrences[idx] = occurrences.get(idx, 0) + 1
-    sums[idx] = sums.get(idx, 0) + charge[i]
-    
-    if ID_channel[i] % 2 != 0:
-        occurrences_HG[idx] = occurrences_HG.get(idx, 0) + 1
-        sums_HG[idx] = sums_HG.get(idx, 0) + charge[i]
-        sums_HG_CN[idx] = sums_HG_CN.get(idx, 0) + charge[i]/LivePMTs[i]
+print("-- Saving results into output rootfile")
+results = pd.DataFrame({
+    'Charge': total_charge,
+    'Fired_PMTs': Fired_PMTs,        
+    'Charge_Norm': normalized_charge,
+    'x_CM': x_CM,
+    'y_CM': y_CM,
+    'z_CM': z_CM,
+    'TOF' : TOF,
+    'WF_RiseTime' : WF_RiseTime_per_event,
+    'WF_RiseTime_diff' : WF_RiseTime_diff_per_event
+})
 
+output_filename = sys.argv[2]
 
-for idx in sums:
-    sums[idx] /= occurrences[idx]
-
-occurrences_counts = list(occurrences.values())
-sums_values = list(sums.values())
-sums_values = [-x for x in sums_values]
-
-occurrences_counts_HG = list(occurrences_HG.values())
-sums_values_HG = list(sums_HG.values())
-sums_values_HG = [-x for x in sums_values_HG]
-
-sums_values_HG_CN = list(sums_HG_CN.values())
-sums_values_HG_CN = [-x for x in sums_values_HG_CN]
-
-
-x_CM_array = np.array(list(x_CM_per_numero.values())) / 1000
-y_CM_array = np.array(list(y_CM_per_numero.values())) / 1000
-z_CM_array = np.array(list(z_CM_per_numero.values())) / 1000
-
-
-#### PLOTTING
-
-# Reconstructed energy spectra + scatter plot firedPMTs:charge
-plt.figure(figsize=(10, 5))
-
-plt.subplot(1, 3, 1)
-plt.hist(occurrences_counts_HG, bins=max(occurrences_counts_HG)+1, color='blue', edgecolor='blue', linewidth=1.2)
-plt.xlabel('"Fired" Waveforms')
-plt.ylabel('events')
-plt.yscale('log')
-plt.title('"Fired" PMTs histogram')
-
-plt.subplot(1, 3, 2)
-plt.hist(sums_values_HG, bins=1000, range=(0,1e6), color='blue', edgecolor='blue', linewidth=1.2)
-plt.xlabel('Charge [A.U.]')
-plt.ylabel('Events')
-plt.yscale('log')
-plt.title('Charge histogram, high gain channels')
-
-print(len(occurrences_counts_HG))
-print(len(sums_values_HG))
-
-plt.subplot(1, 3, 3)
-range_xy = [[0,80], [0,1e6]]
-plt.hist2d(occurrences_counts_HG, sums_values_HG, bins = 80, range=range_xy, cmap='jet')
-plt.xlabel('Fired PMTs')
-plt.ylabel('Charge [A.U.]')
-
-
-# Reconstructed coordinate: scatter plots
-plt.figure(figsize=(10, 5))
-
-x_bins = np.linspace(x_CM_array.min(), x_CM_array.max(), 200)
-y_bins = np.linspace(y_CM_array.min(), y_CM_array.max(), 200)
-z_bins = np.linspace(y_CM_array.min(), y_CM_array.max(), 200)
-
-x_range = [np.percentile(x_CM_array, 4), np.percentile(x_CM_array, 96)]
-y_range = [np.percentile(y_CM_array, 4), np.percentile(y_CM_array, 96)]
-z_range = [np.percentile(z_CM_array, 4), np.percentile(z_CM_array, 96)]
-
-r_CM_array = np.sqrt(x_CM_array**2 + y_CM_array**2 + z_CM_array**2)
-
-range_xy = [[-4.3, 4.3], [-4.3, 4.3]]
-
-plt.subplot(1, 3, 1)
-plt.hist2d(x_CM_array, y_CM_array, bins = 100, range=range_xy, cmap='jet')
-plt.xlabel('x [m]')
-plt.ylabel('y [m]')
-
-plt.subplot(1, 3, 2)
-plt.xlabel('y [m]')
-plt.ylabel('z [m]')
-plt.hist2d(y_CM_array, z_CM_array, bins = 100, range=range_xy, cmap='jet')
-
-plt.subplot(1, 3, 3)
-plt.xlabel('x [m]')
-plt.ylabel('z [m]')
-plt.hist2d(x_CM_array, z_CM_array, bins = 100, range=range_xy, cmap='jet')
-
-plt.tight_layout()
-
-# Reconstructed coordinate: 1D Histos
-plt.figure(figsize=(10,5))
-
-
-plt.subplot(2, 2, 1)
-plt.hist(x_CM_array, bins = 200, range=(-3,3) )
-plt.xlabel('x [m]')
-plt.subplot(2, 2, 2)
-plt.xlabel('y [m]')
-plt.hist(y_CM_array, bins = 200, range=(-3,3))
-plt.subplot(2, 2, 3)
-plt.hist(z_CM_array, bins = 200, range=(-3,3))
-plt.xlabel('z [m]')
-plt.subplot(2, 2, 4)
-plt.hist(r_CM_array, bins = 200, range=(0,5e0))
-plt.xlabel('r [m]')
-#plt.show()
-
-plt.figure(figsize=(13,7))
-
-
-plt.subplot(2,3,1)
-plt.hist(WF_RiseTime_diff_aligned, bins = 200, range = (-100,700) )
-plt.xlabel('t scint aligned [ns]')
-plt.ylabel('entries')
-plt.subplot(2,3,4)
-plt.hist(WF_RiseTime_diff_aligned, bins = 200, range = (-100,700) )
-plt.yscale('log')
-plt.xlabel('t scint aligned [ns]')
-plt.ylabel('entries [log scale]')
-plt.subplot(2,3,2)
-plt.hist(WF_RiseTime_diff, bins = 200, range = (100,600) , color = "purple")
-plt.xlabel('t scint [ns]')
-plt.ylabel('entries')
-plt.subplot(2,3,5)
-plt.hist(WF_RiseTime_diff, bins = 200, range = (100,600) , color = "purple")
-plt.yscale('log')
-plt.xlabel('t scint [ns]')
-plt.ylabel('entries [log scale]')
-
-plt.subplot(2,3,3)
-plt.hist(TOF, bins = 200, range = (-5,40) , color = "darkorange")
-plt.xlabel('TOF [ns]')
-plt.yscale('log')
-plt.ylabel('entries')
-
-plt.subplot(2,3,6)
-plt.hist(min_WF_RiseTime_per_evento, bins = 200, range = (100,600) , color = "seagreen" )
-plt.xlabel('minimum time rise (for each event)')
-plt.ylabel('entries')
-plt.tight_layout()
-#plt.show()
-
-Charge_Norm = np.array(Charge_Norm)
-
-print(Charge_Norm)
-print(Charge_Norm.size)
-print(Charge_Norm.shape)
-
-output_filename = "output.root"
-
-# Apre il file ROOT in modalità scrittura
 with uproot.recreate(output_filename) as f:
-    # Crea un nuovo file
     f["RecEvents"] = {
-    	"index" : np.unique(index),
-        "x": x_CM_array,
-        "y": y_CM_array,
-        "z": z_CM_array,
-        "r": r_CM_array,
-        "FiredPMTs": occurrences_counts_HG,
-        "Charge": np.array(sums_values_HG)/1000,
-        "Charge_Norm" : np.array(sums_values_HG_CN)/1000
+        "Index": results.index.to_numpy(),
+        "x": results['x_CM'].to_numpy() if 'x_CM' in results else None,
+        "y": results['y_CM'].to_numpy() if 'y_CM' in results else None,
+        "z": results['z_CM'].to_numpy() if 'z_CM' in results else None,
+        "Fired_PMTs": results['Fired_PMTs'].to_numpy(),
+        "Charge": results['Charge'],
+        "Charge_Norm": results['Charge_Norm'],
+        "TOF": results['TOF'],
+        'WF_RiseTime' : results['WF_RiseTime'],
+        'WF_RiseTime_diff' : results['WF_RiseTime_diff']
     }
 
-print("ROOT file:", output_filename)
+print(f"Output ROOT file created: {output_filename}")
+
+
+
+
+#plt.figure(figsize=(10, 5))
+#plt.hist(results['WF_RiseTime_diff'], bins=100, color='blue', edgecolor='blue', linewidth=1.2)
+#plt.xlabel('Charge_Norm [A.U.]')
+#plt.ylabel('Events')
+#plt.yscale('log')
+#plt.title('Charge_Norm Histogram')
+#plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WaveformAnalyzer.py
+++ WaveformAnalyzer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# OSIRIS WAVEFORM BASIC ANALYZER
-# Author: Davide Basilico davide.basilico@mi.infn.it  2024 April 05
+# OSIRIS WAVEFORM BASIC ANALYZER  v.0.1.0
+# Author: Davide Basilico davide.basilico@mi.infn.it  2024 May 06
+
 import sys
 import uproot
 import re
@@ -11,26 +12,26 @@ import awkward as ak
 from datetime import datetime
 from Waveform import Waveform
 from tqdm import tqdm
-from utils import extract_date_and_formatted_date, read_all_PMTs_coordinates, load_PMT_coordinates, find_PMT_Coordinates, print_progress
-
-synthetic_params = {
-    'flat_height': 1.0,            # Altezza della componente piatta
-    'gaussian_amplitude': 5.0,     # Amplitude della gaussiana
-    'gaussian_center': 50,         # Centro della gaussiana
-    'gaussian_width': 10           # Larghezza della gaussiana
-}
+from utils import (
+    extract_date_and_formatted_date,
+    read_all_PMTs_coordinates,
+    load_PMT_coordinates,
+    find_PMT_Coordinates,
+    print_progress,
+    generate_synthetic_waveform
+)
+from numba import njit,jit
 
 def main():
 
-	if len(sys.argv) != 4:
+	synth_mode = 'false'
+	
+	if len(sys.argv) < 3:
 		print("Usage: python WF_Analyzer.py [Input_RootFile] [Output_TxtFile]")
 		sys.exit(1)
-
-	file = uproot.open(sys.argv[1])
-	tree = file['EventTree']
-	Entries = int(tree.num_entries) - 1 
-	
-	Entries = 2000
+		
+	header_synth = "name	date	index	charge	WF_RiseTime	\n"
+	header = "name\tdate\tindex\tcharge\tGCU\tWF_RiseTime\ttrgNsec\tID_channel\tGCUID\tx_PMT\ty_PMT\tz_PMT\tLivePMTs\tgain\n"
 
 	# Read all the PMTs coordinates (x,y,z)
 	all_PMTs_x, all_PMTs_y, all_PMTs_z, all_PMTs_gain_corr = read_all_PMTs_coordinates("OSIRIS_cable_map_N.conf")
@@ -38,84 +39,69 @@ def main():
 	with open(sys.argv[2], 'w'):
 		pass
 
-	extracted_string, extracted_date = extract_date_and_formatted_date(sys.argv[1])
+	print("### Welcome to WaveformAnalyzer ###")		
 
-	print("### Welcome to WaveformAnalyzer ###")	
-	print("Run: " , extracted_string, "\nDate:", extracted_date)
+	if(synth_mode == 'false'):
 	
-	synth_mode = 0
-	synth_params = {}	
-	if synth_mode == 1:
+		block_size = 100
+		file = uproot.open(sys.argv[1])
+		tree = file['EventTree']
+		Entries = int(tree.num_entries) - 1 
+	
+		extracted_string, extracted_date = extract_date_and_formatted_date(sys.argv[1])
+		print("Run: " , extracted_string, "\nDate:", extracted_date)		
+		with open(sys.argv[2], 'a') as file:
+			file.write(header)
+	
+			for start in tqdm(range(0, Entries, block_size), desc="Processing", leave=True):
+				stop = min(start + block_size, Entries)
+				events = tree.arrays(["eventId", "trgNsec", "IDdata.samples", "IDdata.GCUID", "IDdata.channelID"], entry_start=start, entry_stop=stop)
+
+				for j in range(stop - start):
+					trgNsec = ak.to_numpy(events["trgNsec"][j])
+					IDdata_GCUID = ak.to_numpy(events["IDdata.GCUID"][j])
+					IDdata_channelID = ak.to_numpy(events["IDdata.channelID"][j])
+					IDdata_samples_vector = ak.to_numpy(events["IDdata.samples"][j])
+
+					for i in range(IDdata_channelID.shape[0]):
+						if IDdata_channelID[i] % 2 == 0:  # Only high-gains
+							continue
+										
+						waveform = Waveform(IDdata_samples_vector[i, :], threshold_method='std_dev', baseline_entries=50)
+						fired_PMTs, integrated_charge, idx = waveform.analyze_waveform()
+
+						if fired_PMTs > 0:
+							coordinate, gain_corr = find_PMT_Coordinates(IDdata_GCUID[i], IDdata_channelID[i])
+							integrated_charge = integrated_charge / float(gain_corr) * 100.
+						
+							file.write(f"{extracted_string}\t{extracted_date}\t{start+j}\t{integrated_charge}\t{i}\t{idx}\t{trgNsec[0]}\t{IDdata_channelID[i]}\t{IDdata_GCUID[i]}\t{coordinate[0]}\t{coordinate[1]}\t{coordinate[2]}\t{int(IDdata_channelID.shape[0]/2)}\t{gain_corr}\n")
+						
+
+	if(synth_mode == 'true'):
+	
+		print("Synth mode on: generating synthetic waveforms")	
+		
+		extracted_string, extracted_date = '2024_X', '2024_MM_DD'
+			
 		synth_params = {
-		'flat_height': 11000,            
-		'gaussian_amplitude': -100,     
-		'gaussian_center': 250,         
-		'gaussian_width': 10           
-		}
-
-
-	with open(sys.argv[2], 'a') as file:
-		for j in tqdm(range(0, Entries), total=Entries, desc="Processing", leave=True,   ):
-			
-			if synth_mode == 1:
-				samples = generate_synthetic_waveform(synth_params)
-				
-				waveform = Waveform(samples, threshold_method='std_dev', baseline_entries=50)
-				
-				fired_PMTs, integrated_charge, idx = waveform.analyze_waveform()
-				
-				print(j,fired_PMTs, integrated_charge, idx)
-			
-			if synth_mode == 0:
-				events = tree.arrays(["eventId","trgNsec","IDdata.samples","IDdata.GCUID","IDdata.channelID"], entry_start=j, entry_stop=j+1)
-				trgNsec = ak.to_numpy(events["trgNsec"])
-				IDdata_GCUID = ak.to_numpy(events["IDdata.GCUID"])
-				IDdata_channelID = ak.to_numpy(events["IDdata.channelID"])			
-				IDdata_samples_vector = ak.to_numpy(events["IDdata.samples"])
-
-				for i in range(0, IDdata_channelID.shape[1]-1):
-				
-
-					if IDdata_channelID[0,i] % 2 == 0:  # Only high-gains
-						continue
-
-					waveform = Waveform(IDdata_samples_vector[0, i, :], threshold_method='std_dev', baseline_entries=50)
+		'flat_height': 11000,			
+		'gaussian_amplitude': -100,	 
+		'gaussian_center': 250,		 
+		'gaussian_width': 10		   
+		}		
+		
+		with open(sys.argv[2], 'a') as file:
+			file.write(header_synth)	
+			Entries = int(sys.argv[3])	
+			for start in tqdm(range(0, Entries), desc="Processing", leave=True):
 					
-					#waveform = Waveform(samples, synthetic_waveform_params=synthetic_params)
-	
-					fired_PMTs, integrated_charge, idx = waveform.analyze_waveform()
+				samples = generate_synthetic_waveform(synth_params)			
+				waveform = Waveform(samples, threshold_method='std_dev', baseline_entries=50)
+				fired_PMTs, integrated_charge, idx = waveform.analyze_waveform()
+				#print(start,fired_PMTs, integrated_charge, idx)
+				
+				file.write(f"{extracted_string}\t{extracted_date}\t{start}\t{integrated_charge}\t{idx}\n")
 
-
-					if fired_PMTs > 0:
-						coordinate, gain_corr = find_PMT_Coordinates(IDdata_GCUID[0,i], IDdata_channelID[0,i])
-						integrated_charge = integrated_charge / float(gain_corr) * 100.
-						#print(IDdata_GCUID[0,i], IDdata_channelID[0,i], coordinate, gain_corr)
-						#print(gain_corr)
-						with open(sys.argv[2], 'a') as file:
-							file.write(f"{extracted_string}\t{extracted_date}\t{j}\t{integrated_charge}\t{i}\t{idx}\t{trgNsec[0]}\t{IDdata_channelID[0, i]}	\t{IDdata_GCUID[0, i]}\t{coordinate[0]}\t{coordinate[1]}\t{coordinate[2]}\t{int(IDdata_channelID.shape[1]/2)}\t{gain_corr}\n")
-
-
-def generate_synthetic_waveform(params):
-    flat_height = params['flat_height']
-    gaussian_amplitude = params['gaussian_amplitude']
-    gaussian_center = params['gaussian_center']
-    gaussian_width = params['gaussian_width']
-
-    # Generazione della waveform sintetica
-    x = np.arange(600)
-    # Creazione del rumore di fondo con fluttuazioni casuali
-    background_noise = np.random.normal(0, 5, size=600)  # Deviazione standard di 0.1 per esempio
-    flat_component = np.ones_like(x) * flat_height + background_noise
-    
-    # Generazione del contributo gaussiano con fluttuazioni casuali
-    gaussian_noise = np.random.normal(0, 0.05, size=600)  # Deviazione standard di 0.05 per esempio
-    gaussian_component = np.exp(-0.5 * ((x - gaussian_center) / gaussian_width)**2) * (gaussian_amplitude + gaussian_noise)
-    
-    synthetic_samples = flat_component + gaussian_component
-    
-    #print(synthetic_samples)
-
-    return synthetic_samples
 
 if __name__ == "__main__":
 	time_start = time.time()
