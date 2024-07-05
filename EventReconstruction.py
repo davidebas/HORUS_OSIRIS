4d6cf76f4698ecaf360a0f164bcdc56860fd56b1
davidebas <davide.basilico@mi.infn.it>
2024-07-05 20:04:27 +0200

    HORUS Event Reconstruction v0.2

--- EventReconstruction.py
+++ EventReconstruction.py
@@ -6,114 +6,209 @@ import uproot
 from tqdm import tqdm
 from tqdm.auto import tqdm
 tqdm.pandas()
+import os
+import argparse
+from argparse import RawTextHelpFormatter
 
 c = 3.0e8
 n_LS = 1.55
 
-print("### Event Reconstruction ###")		
+def parse_arguments():
+    prs = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    prs.add_argument("-i", "--InputFile", default="null", help="Input file name")
+    prs.add_argument("-o", "--OutputFile", default="null", help="Output file name; default: rec-input_file.root")
+    prs.add_argument("-m", "--muon", default="true", help="Apply muon veto; default: true")
+    prs.add_argument("-a", "--All", default="false", help="Analyse all the data file with a specific pattern; default: false")
+    prs.add_argument("-d", "--Dir", default="false", help="Directory containing files to be analyzed; default: false")
+    prs.add_argument("-Muon_Veto_Threshold", "--Threshold_OD_Fired", default=5, help="Muon veto threshold for OD multiplicity; default: 5")    
+    return prs.parse_args()
 
-def weighted_center(group, coord):
-    return (group[coord] * group['charge'].abs()).sum() / group['charge'].abs().sum()
+def load_data(input_file, all_files, folder_path):
+    df_list = []
 
-plt.rcParams.update({'font.size': 12})
+    if input_file != "null" and all_files == "false":
+        data_unclean = pd.read_csv(input_file, delimiter='\t')
+        print("Open only: ", input_file)
+        return data_unclean
 
-data_unclean = pd.read_csv(sys.argv[1], delimiter='\t')
+    if all_files != "false":
+    	folder = os.path.dirname(input_file)
+    	prefix = os.path.basename(input_file).rsplit('_', 1)[0]  # Ottieni il prefisso dal nome del file di input
 
-data = data_unclean.replace([np.inf, -np.inf], np.nan).dropna(subset=['charge'])
-
-grouped = data.groupby('index')
-total_charge = -grouped['charge'].sum()
-average_live_pmts = grouped['LivePMTs'].mean()
-trgTime = grouped['trgTime'].mean()
-trgTime_diff = trgTime.diff().shift(-1)
-normalized_charge = total_charge / average_live_pmts
-Fired_PMTs = grouped.size()
-
-weights = data['charge'].abs()
-
-print("-- Position reco based on charge barycenter")
-x_CM = (data['x_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
-y_CM = (data['y_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
-z_CM = (data['z_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
-
-data = data.merge(x_CM.rename('x_CM'), on='index')
-data = data.merge(y_CM.rename('y_CM'), on='index')
-data = data.merge(z_CM.rename('z_CM'), on='index')
-
-print("-- TOF calculation")
-data['TOF'] = np.sqrt((data['x_CM'] - data['x_PMT'])**2 +
-                      (data['y_CM'] - data['y_PMT'])**2 +
-                      (data['z_CM'] - data['z_PMT'])**2) / 1000 / n_LS / c * 1.0e9
-
-data['WF_RiseTime_diff'] = data['WF_RiseTime'] - data['TOF']                      
-
-TOF = data.groupby('index')['TOF'].apply(list)
-WF_RiseTime_diff_per_event = data.groupby('index')['WF_RiseTime_diff'].apply(list)
-WF_RiseTime_per_event = data.groupby('index')['WF_RiseTime'].apply(list)
-
-print("-- Saving results into output rootfile")
-results = pd.DataFrame({
-    'Charge': total_charge,
-    'Fired_PMTs': Fired_PMTs,        
-    'Charge_Norm': normalized_charge,
-    'x_CM': x_CM,
-    'y_CM': y_CM,
-    'z_CM': z_CM,
-    'trgTime' : trgTime,
-    'trgTime_diff' : trgTime_diff,
-    'TOF' : TOF,
-    'WF_RiseTime' : WF_RiseTime_per_event,
-    'WF_RiseTime_diff' : WF_RiseTime_diff_per_event
-})
-
-#for idx, row in results.iterrows():
-#    print(f"Event index: {idx}, integrated charge: {row['Charge']}")
-
-output_filename = sys.argv[2]
-
-with uproot.recreate(output_filename) as f:
-    f["RecEvents"] = {
-        "Index": results.index.to_numpy(),
-        "x": results['x_CM'].to_numpy() if 'x_CM' in results else None,
-        "y": results['y_CM'].to_numpy() if 'y_CM' in results else None,
-        "z": results['z_CM'].to_numpy() if 'z_CM' in results else None,
-        "Fired_PMTs": results['Fired_PMTs'].to_numpy(),
-        "Charge": results['Charge'],
-        "Charge_Norm": results['Charge_Norm'],
-        'trgTime' : trgTime,
-        'trgTime_diff' : trgTime_diff,
-        "TOF": results['TOF'],
-        'WF_RiseTime' : results['WF_RiseTime'],
-        'WF_RiseTime_diff' : results['WF_RiseTime_diff']
-    }
-
-print(f"Output ROOT file created: {output_filename}")
+    	files = [f for f in os.listdir(folder) if f.startswith(prefix)]
+    	files.sort()
+    	for i, filename in enumerate(files):
+    	    file_path = os.path.join(folder, filename)
+    	    df = pd.read_csv(file_path, delimiter='\t')
+    	    if i != 0:
+    	        df["index"] = df["index"] + df_list[-1]["index"].max() + 1
+    	    df_list.append(df)
+    	    print("Loading: ", filename)
+    	    print("Events from ", df["index"].min(), " to ", df["index"].max())
+    	data_unclean = pd.concat(df_list, ignore_index=True)
+    	return data_unclean
 
 
+    if folder_path != "false":
+        files = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+        files.sort()
+        for i, filename in enumerate(files):
+            file_path = os.path.join(folder_path, filename)
+            df = pd.read_csv(file_path, delimiter='\t')
+            if i != 0:
+                df["index"] = df["index"] + df_list[-1]["index"].max() + 1
+            df_list.append(df)
+            print(f"Open: {filename}")
+            print(df["index"].min(), df["index"].max())
+        data_unclean = pd.concat(df_list, ignore_index=True)
+        print(data_unclean["index"].max())
+        return data_unclean
 
+    return None
 
-#plt.figure(figsize=(10, 5))
-#plt.hist(results['WF_RiseTime_diff'], bins=100, color='blue', edgecolor='blue', linewidth=1.2)
-#plt.xlabel('Charge_Norm [A.U.]')
-#plt.ylabel('Events')
-#plt.yscale('log')
-#plt.title('Charge_Norm Histogram')
-#plt.show()
+def process_data(data):
+    data = data.replace([np.inf, -np.inf], np.nan).dropna(subset=['charge'])
+    grouped = data.groupby('index')
+    total_charge = -grouped['charge'].sum()
+    average_live_pmts = grouped['LivePMTs'].mean()
+    trgTime = grouped['trgTime'].mean()
+    trgTime_diff = trgTime.diff().shift(-1)
+    normalized_charge = total_charge / average_live_pmts
+    Fired_PMTs = grouped.size()
+    OD_fired = grouped['OD'].sum()
+    Shape_Ch = grouped['Shape_Ch'].max()
 
+    data_OD = data[data['OD'] == 1]
+    grouped_OD = data_OD.groupby('index')
+    total_charge_OD = -grouped_OD['charge'].sum()
+    average_live_pmts_OD = grouped_OD['LivePMTs'].mean()
+    normalized_charge_OD = total_charge_OD / average_live_pmts_OD
 
+    data_ID = data[data['OD'] == 0]
+    grouped_ID = data_ID.groupby('index')
+    total_charge_ID = -grouped_ID['charge'].sum()
+    average_live_pmts_ID = grouped_ID['LivePMTs'].mean()
+    normalized_charge_ID = total_charge_ID / average_live_pmts_ID
 
+    weights = data['charge'].abs()
 
+    print("-- Position reco based on charge barycenter")
+    x_CM = (data['x_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
+    y_CM = (data['y_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
+    z_CM = (data['z_PMT'] * weights).groupby(data['index']).sum() / weights.groupby(data['index']).sum()
 
+    data = data.merge(x_CM.rename('x_CM'), on='index')
+    data = data.merge(y_CM.rename('y_CM'), on='index')
+    data = data.merge(z_CM.rename('z_CM'), on='index')
 
+    print("-- TOF calculation")
+    data['TOF'] = np.sqrt((data['x_CM'] - data['x_PMT']) ** 2 +
+                          (data['y_CM'] - data['y_PMT']) ** 2 +
+                          (data['z_CM'] - data['z_PMT']) ** 2) / 1000 / n_LS / c * 1.0e9
 
+    TOF = data.groupby('index')['TOF'].apply(list)
+    data['WF_RiseTime_diff'] = data['WF_RiseTime'] - data['TOF']
+    WF_RiseTime_diff_per_event = data.groupby('index')['WF_RiseTime_diff'].apply(list)
+    WF_RiseTime_per_event = data.groupby('index')['WF_RiseTime'].apply(list)
+    mean_WF_RiseTime_per_event = data.groupby('index')['WF_RiseTime'].mean()
 
+    trgTime_aligned = grouped['trgTime'].mean() - (mean_WF_RiseTime_per_event - 250) * 1e-9
+    trgTime_diff_aligned = trgTime_aligned.diff().shift(-1)
 
+    results = pd.DataFrame({
+        'Charge': total_charge,
+        'Fired_PMTs': Fired_PMTs,
+        'Charge_Norm': normalized_charge,
+        'Charge_Norm_OD': normalized_charge_OD,
+        'Charge_Norm_ID': normalized_charge_ID,
+        'x_CM': x_CM,
+        'y_CM': y_CM,
+        'z_CM': z_CM,
+        'trgTime': trgTime,
+        'trgTime_diff': trgTime_diff,
+        'trgTime_aligned': trgTime_aligned,
+        'trgTime_diff_aligned': trgTime_diff_aligned,
+        'TOF': TOF,
+        'WF_RiseTime': WF_RiseTime_per_event,
+        'WF_RiseTime_diff': WF_RiseTime_diff_per_event,
+        'OD_fired': OD_fired,
+        'Shape_Ch': Shape_Ch
+    })
+    return results
 
+def apply_muon_veto(results,Threshold_OD_Fired):
+    print("-- Applying the muon veto")
+    od_mult = results['OD_fired'] >= Threshold_OD_Fired
+    events_to_remove = results[od_mult]
 
+    time_threshold = 20E-6
+    for i, (index, row) in enumerate(tqdm(events_to_remove.iterrows(), desc="\tApplying time cut: ")):
+        if i == 0:
+            print("\n\n\tIt could take time. This is the total number of iterations: ", len(events_to_remove.index.to_numpy()), ". Be patient...\n")
+        time_diff_condition = (results['trgTime'] - row['trgTime']).abs() < time_threshold
+        results = results.loc[~time_diff_condition]
 
+    results = results.loc[~od_mult]
+    return results
 
+def calculate_energies(results):
+    print("-- Calculating Energy_Prompt and Energy_Delayed")
+    results['Energy_Prompt'] = results['Charge_Norm'] / 3800.
+    results['Energy_Delayed'] = results['Charge_Norm'].shift(-1) / 3800.
+    results = results.dropna(subset=['Energy_Prompt'])
+    return results
 
+def save_results(results, output_file, input_file, all_files, folder_path):
+    if output_file == "null":
+        if all_files == "true" or folder_path != "false":
+            output_filename = "rec-" + input_file[:-9] + ".root"
+        else:
+            output_filename = "rec-" + input_file[:-4] + ".root"
+    else:
+        output_filename = output_file
 
+    with uproot.recreate(output_filename) as f:
+        f["RecEvents"] = {
+            "Index": results.index.to_numpy(),
+            "x": results['x_CM'].to_numpy() if 'x_CM' in results else None,
+            "y": results['y_CM'].to_numpy() if 'y_CM' in results else None,
+            "z": results['z_CM'].to_numpy() if 'z_CM' in results else None,
+            "Fired_PMTs": results['Fired_PMTs'].to_numpy(),
+            "Charge": results['Charge'],
+            "Charge_Norm": results['Charge_Norm'],
+            "Charge_Norm_OD": results['Charge_Norm_OD'],
+            "Charge_Norm_ID": results['Charge_Norm_ID'],
+            'trgTime': results['trgTime'],
+            'trgTime_diff': results['trgTime_diff'],
+            'trgTime_aligned': results['trgTime_aligned'],
+            'trgTime_diff_aligned': results['trgTime_diff_aligned'],
+            "TOF": results['TOF'],
+            'WF_RiseTime': results['WF_RiseTime'],
+            'WF_RiseTime_diff': results['WF_RiseTime_diff'],
+            'Energy_Prompt': results['Energy_Prompt'].to_numpy(),
+            'Energy_Delayed': results['Energy_Delayed'].to_numpy(),
+            'OD_fired': results['OD_fired'],
+            'Shape_Ch': results['Shape_Ch']
+        }
+    print(f"Output ROOT file created: {output_filename}")
 
+def main():
+    args = parse_arguments()
 
+    data_unclean = load_data(args.InputFile, args.All, args.Dir)
+    if data_unclean is None:
+        print("No data loaded.")
+        return
+
+    print("### Event Reconstruction ###")
+    results = process_data(data_unclean)
+
+    if args.muon == "true":
+        results = apply_muon_veto(results,args.Threshold_OD_Fired)
+
+    results = calculate_energies(results)
+    save_results(results, args.OutputFile, args.InputFile, args.All, args.Dir)
+
+if __name__ == "__main__":
+    main()
 
--- README.md
+++ README.md
@@ -1,8 +1,40 @@
 # HORUS: Hybrid OSIRIS data Reconstruction and Understanding Software
-Basic event analyzer for OSIRIS (waveform analysis + event reconstruction) with additional tools for the determination of background levels.
+- v0.2
+- 2024 July 05
+- Authors: Davide Basilico (davide.basilico@mi.infn.it) and Marco Beretta (marco.beretta@mi.infn.it)
+- Basic event analyzer for OSIRIS (waveform analysis + event reconstruction) with additional tools for the determination of background levels.
 
--- Waveform analysis:
-python3 WaveformAnalyzer.py InputRootfileFromEBParser.root OutputWaveformAnalyzer.txt
+1) Conversion from binary format to ROOTfile: eb2root parser (credits to Kai Loo)
 
--- Event reconstruction:
-python3 EventReconstruction.py OutputWaveformAnalyzer.txt
+./eb2root -i datFile.dat -o RootfileEBParser.root
+
+2) Waveform Analysis:
+
+python3 WaveformAnalyzer.py RootfileEBParser.root Output_WaveformAnalyzer.txt
+
+3) Event reconstruction:
+
+- Position reco based on charge barycenter
+- TOF calculation
+- Applying the muon veto
+
+usage: EventReconstruction.py [-h] [-i INPUTFILE] [-o OUTPUTFILE] [-m MUON] [-a ALL] [-d DIR]
+
+options:
+  -h, --help            show this help message and exit
+  -i INPUTFILE, --InputFile INPUTFILE
+                        Input file name
+  -o OUTPUTFILE, --OutputFile OUTPUTFILE
+                        Output file name; default: rec-input_file.root
+  -m MUON, --muon MUON  Apply muon veto; default: true
+  -a ALL, --All ALL     Analyse all the data file with a specific pattern; default: false
+  -d DIR, --Dir DIR     Directory containing files to be analyzed; default: false
+  
+The output ROOTfile from the EventReconstruction step includes:
+- reconstructed position (coordinates x,y,z) for each event
+- number of overall fired PMTs (Fired_PMTs) and OD fired PMTs (OD_fired) for each event
+- reconstructed energy: Charge, Charge normalized to the number of Live PMTs, Charge normalized to the number of Live PMTs for ID PMTs (Charge, Charge_Norm, Charge_Norm_ID) for each event
+- reconstructed energy for each couple of subsequent events, after the calibration (Charge/3800): Energy_Prompt and Energy_Delayed
+- Time of flight for each fired PMT: TOF
+- Rise Time for each fired PMT (WF_RiseTime) and the subtracted Rise Time - TOF (WF_RiseTime_diff)
+- number of channels for each event (Shape_Ch)
